289f356c281de31471f5bce957ab696b01e8dad8
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-12-20 19:24:50 -0500

    move to single makefile build

---
+++ src/Makefile.mk
@@ -0,0 +1,24 @@
+AM_CPPFLAGS = \
+-Isrc \
+-I$(top_srcdir) \
+-I$(includedir) \
+-DLOCALEDIR=\"$(datadir)/locale\" \
+-DPACKAGE_DATA_DIR=\"$(module_dir)/$(PACKAGE)\" \
+@E_CFLAGS@
+
+pkgdir = $(module_dir)/$(PACKAGE)/$(MODULE_ARCH)
+pkg_LTLIBRARIES = module.la
+module_la_SOURCES = \
+src/e_mod_main.h \
+src/e_mod_main.c \
+src/ds_config.c \
+src/maximize.c \
+src/moveresize.c \
+src/pip.c \
+src/zoom.c \
+src/magnify.c \
+src/desksanity.c
+
+module_la_LIBADD = @E_LIBS@
+module_la_LDFLAGS = -module -avoid-version
+module_la_DEPENDENCIES = $(top_builddir)/config.h
